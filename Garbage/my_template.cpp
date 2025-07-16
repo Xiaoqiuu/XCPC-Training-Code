04ee4c217b6f4f42d903affeba17ded770fbcc20
Xiaoqiuu <xiaoqiuuu@gmail.com>
2025-07-16 23:12:45 +0800

    clean the workspace OvO(#250716)

---
+++ Garbage/my_template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+//#include <queue>
+//#include <vector>
+#define akitama return 0
+#define pq priority_queue
+#define vec vector
+
+using namespace std;
+using ll = long long;
+using ul = unsigned long long;
+using ld = long double;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0),cout.tie(0);
+    ll ans = 0;
+
+    //code here
+
+
+    cout >> ans >> endl;
+    akitama;
+}
