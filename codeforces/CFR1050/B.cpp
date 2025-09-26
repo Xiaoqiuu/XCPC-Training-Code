34c5655dbe702e8070793041a005788378b3ea5b
Xiaoqiuu <xiaoqiuuu@gmail.com>
2025-09-26 09:17:56 +0800

    update: new practice code(#250926)

---
+++ codeforces/CFR1050/B.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <iomanip>
+#include <numeric>
+#include <bitset>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long int;
+/*
+
+*/
+#define akitama return 0
+/*
+Author: Akitama
+Date: 2025-09-23 09:26:00
+Akitama Default Contest Template V2.0
+*/
+
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pb push_back
+#define fi first
+#define se second
+#define l(x) x << 1
+#define r(x) x << 1 | 1
+
+constexpr int N = 1e5 + 7;
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 0X3f3f3f3f;
+
+void solve() {
+    ll n, m, x, y;
+    cin >> n >> m >> x >> y;
+    // vi a(n), b(m);
+    ll cnt = 0;
+    for (int i = 0; i < n; ++ i) { ll _x; cin >> _x; cnt += (_x < x? 1 : 0); }
+    for (int i = 0; i < m; ++ i) { ll _y; cin >> _y; cnt += (_y < y? 1 : 0); }
+    cout << n + m << '\n';
+}
+
+signed main() {
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    int _ = 1; cin >> _;
+    while (_--){
+        solve();
+    }
+    akitama;
+}
---
+++ codeforces/CFR1050/C.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <iomanip>
+#include <numeric>
+#include <bitset>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long int;
+/*
+
+*/
+#define akitama return 0
+/*
+Author: Akitama
+Date: 2025-09-23 09:44:31
+Akitama Default Contest Template V2.0
+*/
+
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pb push_back
+#define fi first
+#define se second
+#define l(x) x << 1
+#define r(x) x << 1 | 1
+
+constexpr int N = 1e5 + 7;
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 0X3f3f3f3f;
+
+void solve() {
+    int n;
+    ll m;
+    ll ans = 0;
+    cin >> n >> m;
+    vector<pair<ll, ll>> db(n);
+    for (int i = 0; i < n; ++ i) {
+        ll day, b; cin >> day >> b;
+        db[i].fi = day, db[i].se = b;
+    
+        if (i == 0)
+    }
+}   
+
+signed main() {
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    int _ = 1; cin >> _;
+    while (_--){
+        solve();
+    }
+    akitama;
+}
