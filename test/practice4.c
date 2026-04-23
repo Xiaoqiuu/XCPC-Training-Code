e542580401c1cc3c4f8a365a5f87d6c531c21e65
Xiaoqiuu <xiaoqiuuu@gmail.com>
2026-04-23 10:12:33 +0800

    Update: add practice code, main 408 (#260423)

--- test/practice4.c
+++ test/practice4.c
@@ -1,8 +1,16 @@
+/*
+ * @Author: Xiaoqiuu/Akitama
+ * @Date: 2026-04-17 09:08:57
+ * @LastEditors: Xiaoqiuu/Akitama
+ * @LastEditTime: 2026-04-20 23:59:07
+ * @FilePath: /workspace/test/practice4.c
+ * @Description: XCPC | 408 | Unity | Agents
+ */
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #define akitama return 0
 /*
-    List
     Study time: 2026-04-18 09:00:00
 */
 
@@ -11,6 +19,16 @@ typedef struct LNode{
     struct LNode *next;
 }ListNode, *LinkList;
 
-void InitList(ListNode **L){
-    *L = (ListNode*)malloc(sizeof(ListNode));
+bool InitList(LinkList *L){
+    *L = NULL;
+    return true;
+}
+
+bool InitList2(LinkList *L) {
+    *L = (LinkList)malloc(sizeof(ListNode));
+    if (*L == NULL) {
+        return false;
+    }
+    (*L)->next = NULL;
+    return true;
 }
