d51a1a23484e04fbc65ce38493ba64ddee65ec20
Xiaoqiuu <xiaoqiuuu@gmail.com>
2026-06-03 14:40:37 +0800

    Update: Add Practice Code(408)(#260603)

---
+++ 408/WD_DS/5/5-4-1-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define akitama return 0
+
+typedef struct CSNode {
+    char data;
+    struct CSNode *firstchild;
+    struct CSNode *nextsibing;
+} CSNode, *CSTree;
+
+signed main() {
+    
+}
