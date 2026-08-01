8bef490cd2fbbccd118df838e8b551f36e1a0969
Xiaoqiuu <xiaoqiuuu@gmail.com>
2026-08-02 06:44:51 +0800

    Update: Add Practice Code(408)(#260802)

---
+++ 408/WD_DS/2/2-staticLinkList.c
@@ -0,0 +1,21 @@
+/*
+ * @Author: Xiaoqiuu/Akitama
+ * @Date: 2026-07-23 03:10:40
+ * @LastEditors: Xiaoqiuu/Akitama
+ * @LastEditTime: 2026-07-23 03:12:03
+ * @FilePath: /workspace/408/WD_DS/2/2-staticLinkList.c
+ * @Description: XCPC | 408 | Unity | Agents
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct SLinkList{
+    
+} SLinkList;
+
+#define akitama return 0
+
+signed main() {
+
+
+}
